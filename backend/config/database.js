2f71cdd1a85713ab228e9a23831c4db7fbfb1f73
miteshchopda <miteshchopda00@gmail.com>
2026-02-20 13:11:18 +0530

    change host to mongo service

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/expense-tracker';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongo:27017/expense-tracker';
 
 const connectDB = async () => {
   try {
